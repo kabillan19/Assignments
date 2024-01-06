5e11b88d528337578967829b3346e1547378c6c2
Kabillan T A <125804738+kabillan19@users.noreply.github.com>
2024-01-06 23:43:04 +0530

    Uploaded all Java Lab assignments

---
+++ Lab Assignments/labassignment05.java
@@ -0,0 +1,27 @@
+package labassignment;
+
+import java.util.Scanner;
+
+public class labassignment05 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input value: ");
+        double input = in.nextDouble();
+        if (input > 0) {
+            if (input < 1) {
+                System.out.println("Small");
+            } else if (input > 1000000) {
+                System.out.println("Large");
+            } else {
+                System.out.println("Positive Number");
+            }
+        }
+        else if (input < 0 ) {
+            System.out.println("Negative");
+        }
+        else {
+            System.out.println("Zero");
+        }
+    }
+}
---
+++ Lab Assignments/labassignment06.java
@@ -0,0 +1,14 @@
+package labassignment;
+
+import java.util.Scanner;
+
+public class labassignment06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int a = scan.nextInt();
+        int ans = a * a;
+        System.out.println("The Sum of odd Natural Number upto "+ a + " terms is: " + ans );
+
+    }
+}
